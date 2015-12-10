97fa301e72e3cd67218193c7c8e6319a0cf85d07
junjie <1287550045@qq.com>
2015-12-10 13:43:33 +0800

    测试Java文件
    这次它将不会在打印hello world！而是打印hello github！

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class test
 {
 	public static void main(String[] args) {
-		System.out.print("hello world!");
+		//System.out.print("hello world!");
+		System.out.println("hello github!");
 	}
 }
